5b073388ef63278b9697153b995ec9294628f876
fabri <fabritzioalejandrol@gmail.com>
2025-07-27 12:10:21 -0600

    Menu principal

---
+++ actividad9.py
@@ -0,0 +1,12 @@
+peliculas = []
+while True:
+    print("-"*20)
+    print("Catalogo de peliculas")
+    print("1. Agregar peliculas")
+    print("2. Mostrar peliculas registradas")
+    print("3. Buscar peliculas por genero")
+    print("4. Eliminar una película")
+    print("5. Ver estadisticas")
+    print("6. Salir")
+    print("-"*20)
+    opcion = input("Ingrese una de las opciones: ")
