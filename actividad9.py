fa1219ae3892c9ef3daa5dc2692d326b09821374
fabri <fabritzioalejandrol@gmail.com>
2025-07-27 12:31:05 -0600

    Agregar y mostrar peliculas mediante funciones

--- actividad9.py
+++ actividad9.py
@@ -1,7 +1,23 @@
 peliculas = []
+def agregar_peliculas():
+    cantidad_peliculas = int(input("Ingrese cuantas peliculas desea ingresar al catalogo: "))
+    for i in range(cantidad_peliculas):
+        titulo = input("Ingrese el titulo de la pelicula: ")
+        año = int(input("Ingrese el año de estreno: "))
+        genero = input("Ingrese el genero de la pelicula: ")
+        peliculas.append([titulo, año, genero])
+    print("Pelicula agregada")
+def mostrar_peliculas():
+    if not peliculas:
+        print("No hay peliculas agregadas al catalogo")
+        return
+    print("Catalogo de peliculas")
+    for i in peliculas:
+        print(i)
+
 while True:
     print("-"*20)
-    print("Catalogo de peliculas")
+    print("Menu")
     print("1. Agregar peliculas")
     print("2. Mostrar peliculas registradas")
     print("3. Buscar peliculas por genero")
@@ -12,11 +28,6 @@ while True:
     opcion = input("Ingrese una de las opciones: ")
     match opcion:
         case "1":
-            cantidad_peliculas =  int(input("Ingrese cuantas peliculas desea ingresar al catalogo: "))
-            for i in range(cantidad_peliculas):
-                titulo = input("Ingrese el titulo de la pelicula: ")
-                año = int(input("Ingrese el año de estreno: "))
-                genero= input("Ingrese el genero de la pelicula: ")
-                peliculas.append([titulo,año,genero])
-                print("Pelicula agregada")
-
+            agregar_peliculas()
+        case "2":
+            mostrar_peliculas()
