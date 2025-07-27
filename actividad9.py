fa25e207b238958a05b2d2d1e4263c05d66bff15
fabri <fabritzioalejandrol@gmail.com>
2025-07-27 12:19:43 -0600

    Opcion agregar pelicula

--- actividad9.py
+++ actividad9.py
@@ -10,3 +10,13 @@ while True:
     print("6. Salir")
     print("-"*20)
     opcion = input("Ingrese una de las opciones: ")
+    match opcion:
+        case "1":
+            cantidad_peliculas =  int(input("Ingrese cuantas peliculas desea ingresar al catalogo: "))
+            for i in range(cantidad_peliculas):
+                titulo = input("Ingrese el titulo de la pelicula: ")
+                año = int(input("Ingrese el año de estreno: "))
+                genero= input("Ingrese el genero de la pelicula: ")
+                peliculas.append([titulo,año,genero])
+                print("Pelicula agregada")
+
